18573a88b3f528cd96379228facc74dc29cee75e
Orlewilson Maia <orlewilson.maia@gmail.com>
2021-11-28 22:18:53 -0400

    Add exemplos sobre vetor
    Signed-off-by: Orlewilson Maia <orlewilson.maia@gmail.com>

---
+++ exemplos-unidade-1/exemplos-unidade-1/videoaula-3/ex-vetores-1.c
@@ -0,0 +1,24 @@
+/*  Disciplina:     Estrutura de Dados 1 - EAD Fametro
+    Facilitador:    Orlewilson B. Maia
+    Descrição:      Exemplo de declaração de vetor 
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // declaração de vetor
+    int comissao[5];
+
+    // atribuição de valor no vetor
+    comissao[0] = 3500; comissao[1] = 3560;
+    comissao[2] = 4000; comissao[3] = 4000;
+    comissao[4] = 5000;
+
+    // leitura de valor no vetor
+    for (int cont=0; cont < 5; cont++){
+        printf("[%d] = %d \n", cont, comissao[cont]);
+    }
+
+    return 0;
+}
---
+++ exemplos-unidade-1/exemplos-unidade-1/videoaula-3/ex-vetores-2.c
@@ -0,0 +1,19 @@
+/*  Disciplina:     Estrutura de Dados 1 - EAD Fametro
+    Facilitador:    Orlewilson B. Maia
+    Descrição:      Exemplo de vetor com carga inicial automática
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // declaração e inicialização automática de vetor 
+    char vogal[5] = {'a', 'e', 'i', 'o', 'u' };
+
+    // leitura de valor no vetor
+    for (int cont=0; cont < 5; cont++){
+        printf("[%d] = %c \n", cont, vogal[cont]);
+    }
+
+    return 0;
+}
