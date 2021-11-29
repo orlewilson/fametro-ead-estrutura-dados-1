0abeed79e9f0c27779fc72128b45252c968b1c9b
Orlewilson Maia <orlewilson.maia@gmail.com>
2021-11-28 23:46:14 -0400

    Add exemplos registros
    Signed-off-by: Orlewilson Maia <orlewilson.maia@gmail.com>

---
+++ exemplos-unidade-1/exemplos-unidade-1/videoaula-4/ex-registro-1.c
@@ -0,0 +1,29 @@
+/*  Disciplina:     Estrutura de Dados 1 - EAD Fametro
+    Facilitador:    Orlewilson B. Maia
+    Descrição:      Exemplo de cadastro aluno sem usar registro
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // inforações novo aluno
+    char nome[50];
+    char cpf[15];
+    int idade;
+    char telefone[11];
+
+    printf("Escreva seu nome:\n"); gets(nome);
+    printf("Escreva seu CPF:\n"); gets(cpf);
+    printf("Escreva seu telefone:\n"); gets(telefone);
+    printf("Escreva sua idade:\n"); scanf("%d", &idade);
+    
+    printf("--------------------\n");
+    printf("Nome: %s \n", nome);
+    printf("CPF: %s \n", cpf);
+    printf("Telefone: %s \n", telefone);
+    printf("Idade: %d \n", idade);
+    printf("--------------------\n");
+
+    return 0;
+}
---
+++ exemplos-unidade-1/exemplos-unidade-1/videoaula-4/ex-registro-3.c
@@ -0,0 +1,34 @@
+/*  Disciplina:     Estrutura de Dados 1 - EAD Fametro
+    Facilitador:    Orlewilson B. Maia
+    Descrição:      Exemplo de cadastro aluno usando registro com typedef
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // estrutura Aluno
+    typedef struct {
+        char nome[50];
+        char cpf[15];
+        int idade;
+        char telefone[11];
+    } Aluno;
+    
+    // declarando variável do tipo Aluno
+    Aluno novo_aluno;
+
+    printf("Escreva seu nome:\n"); gets(novo_aluno.nome);
+    printf("Escreva seu CPF:\n"); gets(novo_aluno.cpf);
+    printf("Escreva seu telefone:\n"); gets(novo_aluno.telefone);
+    printf("Escreva sua idade:\n"); scanf("%d", &novo_aluno.idade);
+    
+    printf("--------------------\n");
+    printf("Nome: %s \n", novo_aluno.nome);
+    printf("CPF: %s \n", novo_aluno.cpf);
+    printf("Telefone: %s \n", novo_aluno.telefone);
+    printf("Idade: %d \n", novo_aluno.idade);
+    printf("--------------------\n");
+
+    return 0;
+}
