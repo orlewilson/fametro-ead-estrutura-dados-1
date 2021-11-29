bed243cc2c94e0a6c714169052da71ba11a8ae2d
Orlewilson Maia <orlewilson.maia@gmail.com>
2021-11-28 21:24:31 -0400

    Add reorganização dos exemplos da videoaula-1
    Signed-off-by: Orlewilson Maia <orlewilson.maia@gmail.com>

---
+++ exemplos-unidade-1/exemplos-unidade-1/videoaula-1/hello-world.c
@@ -0,0 +1,18 @@
+/*  Disciplina:     Estrutura de Dados 1 - EAD Fametro
+    Facilitador:    Orlewilson B. Maia
+    Descrição:      Hello World em Linguagem C 
+*/
+
+// importação de bibliotecas
+// biblioteca que contém as funções de Entrada e Saída
+#include <stdio.h> 
+
+// função principal
+int main()
+{
+    // código
+    printf("Hello World!!!!\n\n"); 
+
+    // retorno igual a zero indica que deu tudo certo
+    return 0;
+}
